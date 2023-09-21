4bc05014a1979d891a8a523128afb547120ea8dd
Emedo586 <emedopeace99@gmail.com>
2023-09-20 23:38:46 -0700

    Stacks and Queues

---
+++ pop.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * pop - removes the top element of the stack.
+ * @stack: pointer to the top of the stack.
+ * @line_number: line number of opcode.
+ */
+void _pop(stack_t **stack, unsigned int line_number)
+{
+stack_t *top;
+
+if (*stack == NULL) /* check if stack is empty */
+{
+fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+free_stack(*stack);
+exit(EXIT_FAILURE);
+}
+top = *stack; /* store the top element in a temporary variable */
+*stack = (*stack)->next; /* moves stack pointer to the next element */
+if (*stack != NULL) /* if the stack is not empty after popping */
+{
+(*stack)->prev = NULL; /* set the previous pointer of the new top to NULL */
+}
+free(top); /* free the memory of the popped element */
+}
