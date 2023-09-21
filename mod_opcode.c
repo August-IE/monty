634fa0bde95871dab1916c17d68ea284ab64afe7
Emedo586 <emedopeace99@gmail.com>
2023-09-20 23:37:28 -0700

    Stacks and Queues

---
+++ mod_opcode.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+/**
+ * _mod - computes the rest of division of second top element by top element
+ * @stack: pointer to top of stack
+ * @line_number:
+ */
+void _mod(stack_t **stack, unsigned int line_number)
+{
+int top;
+
+if (!stack || !*stack || !(*stack)->next)
+{
+fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
+free_stack(*stack);
+exit(EXIT_FAILURE);
+}
+if ((*stack)->n == 0)
+{
+fprintf(stderr, "L%u: division by zero\n", line_number);
+free_stack(*stack);
+exit(EXIT_FAILURE);
+}
+top = (*stack)->n;
+pop(stack, line_number);
+(*stack)->n %= top;
+}
